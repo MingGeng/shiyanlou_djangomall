aa54398b385530534324f30db20943583be4934e
kuakexing <kuakexing@shiyanlou.com>
2019-03-17 14:58:42 +0800

    add generics

--- djangomall/urls.py
+++ djangomall/urls.py
@@ -1,24 +1,9 @@
-"""djangomall URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.models import User
 
 from rest_framework.decorators import api_view
-from rest_framework import serializers
+from rest_framework import serializers, generics
 from rest_framework.response import Response
 
 class UserSerializer(serializers.ModelSerializer):
@@ -32,7 +17,12 @@ def user_apiview(request):
     data = UserSerializer(user).data
     return Response(data)
 
+class UserList(generics.ListAPIView):
+	queryset = User.objects.all()		
+	serializer_class = UserSerializer
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/1/', user_apiview),
+    path('api/users/', UserList.as_view()),
 ]
