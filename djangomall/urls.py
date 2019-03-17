aee92be1439a36ba892927826e21a57f725381da
kuakexing <kuakexing@shiyanlou.com>
2019-03-17 14:36:17 +0800

    add api settings

--- djangomall/urls.py
+++ djangomall/urls.py
@@ -15,7 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.models import User
+
+from rest_framework.decorators import api_view
+from rest_framework import serializers
+from rest_framework.response import Response
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email',)
+
+@api_view()
+def user_apiview(request):
+    user = User.objects.first()
+    data = UserSerializer(user).data
+    return Response(data)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/1/', user_apiview),
 ]
